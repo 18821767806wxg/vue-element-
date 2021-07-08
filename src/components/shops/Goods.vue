d0a2be25300c94e7016f69ee0fba4b8d5da2fbd1
王小刚 <6514758+xiao_gang_wanghhh@user.noreply.gitee.com>
2021-07-08 17:36:24 +0800

    商品管理：面包屑导航区域

--- src/components/shops/Goods.vue
+++ src/components/shops/Goods.vue
@@ -1,5 +1,10 @@
 <template>
+  <!-- 面包屑导航区域 -->
   <div>
-    <h1>商品列表</h1>
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>商品列表</el-breadcrumb-item>
+    </el-breadcrumb>
   </div>
 </template>
