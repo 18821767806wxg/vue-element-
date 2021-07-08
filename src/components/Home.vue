c0a3e499813306f272eebcd7d6a0f97af0925261
王小刚 <6514758+xiao_gang_wanghhh@user.noreply.gitee.com>
2021-07-08 17:23:59 +0800

    菜单和路由配置

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,19 +1,142 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+  <el-container class="home-container">
+    <!-- 头部区域 -->
+    <el-header>
+      <div>
+        <img src="../assets/heima.png" alt="" />
+        <span>车险核保管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <el-container>
+      <!-- 侧边栏区域 -->
+      <el-aside :width="isCollapse ? '64px' : '200px'">
+        <div class="toggle-button" @click="toggleCollapse">|||</div>
+        <el-menu
+          default-active="2"
+          class="el-menu-vertical-demo"
+          background-color="#333744"
+          text-color="#fff"
+          active-text-color="#409EFF"
+          router
+          :collapse="isCollapse"
+          :unique-opened="true"
+        >
+          <!-- 一级菜单 -->
+          <el-submenu
+            :index="item.id + ''"
+            v-for="item in menulist"
+            :key="item.id"
+          >
+            <!-- 一级菜单的模板区域 -->
+            <template #title>
+              <!-- 图标 -->
+              <i :class="iconsObj[item.id]"></i>
+              <!-- 文本 -->
+              <span>{{ item.authName }}</span>
+            </template>
+
+            <!-- 二级菜单 -->
+            <el-menu-item
+              :index="'/' + data.path"
+              v-for="data in item.children"
+              :key="data.id"
+            >
+              <!-- 二级菜单的模板区域 -->
+              <template #title>
+                <!-- 图标 -->
+                <i class="el-icon-menu"></i>
+                <!-- 文本 -->
+                <span>{{ data.authName }}</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- 内容区域 -->
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      // 左边菜单栏数据
+      menulist: [],
+      iconsObj: {
+        125: 'iconfont icon-user',
+        103: 'iconfont icon-tijikongjian',
+        101: 'iconfont icon-shangpin',
+        102: 'iconfont icon-danju',
+        145: 'iconfont icon-baobiao'
+      },
+      // 是否折叠
+      isCollapse: false
+    }
+  },
+  created() {
+    this.getMenuList()
+  },
   methods: {
     logout() {
       window.sessionStorage.clear()
       this.$router.push('/login')
+    },
+    // 获取所有的菜单 promise要异步接收数据
+    async getMenuList() {
+      const { data: res } = await this.$http.get('menus')
+      if (res.meta.status != '200') this.$message.error(res.meta.msg)
+      this.menulist = res.data
+      console.log('res', this.menulist)
+    },
+    // 是否折叠
+    toggleCollapse() {
+      this.isCollapse = !this.isCollapse
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
+.home-container {
+  height: 100%;
+}
+.el-header {
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+  > div {
+    display: flex;
+    align-items: center;
+    span {
+      padding-left: 15px;
+    }
+  }
+}
+.el-aside {
+  background-color: #333744;
+  .el-menu {
+    // 去除多余边框
+    border-right: none;
+  }
+}
+.el-main {
+  background-color: #eaedf1;
+}
+.toggle-button {
+  background-color: #4a5064;
+  font-size: 10px;
+  line-height: 24px;
+  color: #fff;
+  text-align: center;
+  letter-spacing: 0.2em;
+  cursor: pointer;
+}
 </style>
--- src/router.js
+++ src/router.js
@@ -2,14 +2,33 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from './components/Login.vue'
 import Home from './components/Home.vue'
-
+import Welcome from './components/Welcome.vue'
+import User from './components/user/User.vue'
+import Roles from './components/power/Roles'
+import Rights from './components/power/Rights'
+import Categories from './components/shops/Categories.vue'
+import Goods from './components/shops/Goods.vue'
+import Params from './components/shops/Params.vue'
 Vue.use(Router)
 
 const router = new Router({
   routes: [
     { path: '/', redirect: '/login' },
     { path: '/login', component: Login },
-    { path: '/home', component: Home }
+    {
+      path: '/home',
+      component: Home,
+      redirect: '/welcome',
+      children: [
+        { path: '/welcome', component: Welcome },
+        { path: '/users', component: User },
+        { path: '/roles', component: Roles },
+        { path: '/rights', component: Rights },
+        { path: '/categories', component: Categories },
+        { path: '/goods', component: Goods },
+        { path: '/params', component: Params }
+      ]
+    }
   ]
 })
 
@@ -19,7 +38,9 @@ router.beforeEach((to, from, next) => {
   // from 代表从哪个路径跳转而来
   // next 是一个函数，表示放行
   //     next()  放行    next('/login')  强制跳转
-
+  console.log("5555", to)
+  console.log("5555", from)
+  // debugger
   if (to.path === '/login') return next()
   // 获取token
   const tokenStr = window.sessionStorage.getItem('token')
