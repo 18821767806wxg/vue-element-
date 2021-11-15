6cdf281a7dcc9c439ea201980cdf548d52c59039
18821767806wxg <945497340@qq.com>
2021-11-15 15:55:42 +0800

    123456

--- src/page/Login.vue
+++ src/page/Login.vue
@@ -76,8 +76,8 @@ export default {
     return {
       // 这是登录表单的数据绑定对象
       loginForm: {
-        username: '', //admin
-        password: '' //123456
+        username: 'admin', //admin
+        password: '123456' //123456
       },
       ifExist: '',
       isdisabled: true,
